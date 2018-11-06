4aea981c61afa4ebed333016bf9e411ce84a3464
diogoteixeira19 <44436178+diogoteixeira19@users.noreply.github.com>
2018-11-06 16:02:47 +0000

    Update semaforo.c

--- semaforo.c
+++ semaforo.c
@@ -1,16 +1,39 @@
-/* semaforo.c
+/********************************************************
+ *  semaforo.c
+ * 	Traffic lights with an emergence function in C
+ * 	Solution: state machine with an interrupt request and
+ * 			 timer TC1
+ ********************************************************
  *
+ * 	F=16MHz, T=___. To create a interrupt request every
+ * 	10ms we need to count ______ clock periods.
+ * 	Combination of CP TP and CNT to find a solution to
+ *  CPxTPxCOUNT=________
+ *
+ *  CP=1
+ *
+ *  .
+ *  .
+ *  .
+ *
+ *  CP=1, TP=64, CNT=2500
+ *
+ *
+ *  NORMAL MODE TC1:
+ *  TC1 starts as a given BOTTOM value and counts up to
+ *  65535, and overflows, every time TC1 overflows
+ *  the ISR to timer1 is executed (every 10ms)
+ *
+ ********************************************************
  *  Created on: 14/10/2018
  *      Author: Diogo Teixeira e Bernardo Moreira
- */
-
-/*
- * !COMPLETAR COMENTARIOS!
+ *
  */
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include "timer_tools.h"
+#include "serial_printf.h"
 #include <avr/interrupt.h>
 
 #define RNS PB0
@@ -22,9 +45,9 @@
 
 #define EMR PD2
 
-#define bigT 5000  
-#define smallT 500
-#define mediumT 1000
+#define bigT 5000  /* 50 sec */
+#define smallT 500  /* 5 sec */
+#define mediumT 1000  /* 10 sec */
 
 /* 10ms = 2500 clock cycles @ 16MHz/(1*64) */
 #define T1BOTTOM 65536-2500
@@ -83,7 +106,6 @@ void tc1_init(void){
  *  Define inputs
  *  Set interrupts request at falling edge
  *  Enables INT0
- *  EIFR??
  *  Enable global interrupt flag
  ******************************************************/
 void init(void){
@@ -102,6 +124,9 @@ int main(void)
 {
 
   init(); /*INICIALIZAÇÃO*/
+  printf_init();
+
+  //printf("hello world");
 
   while (1)
   {
@@ -111,6 +136,8 @@ int main(void)
 				tc1_init();
 				state=1;
 				pstate=1;
+				printf("\n estado %d -> VERDE - VERMELHO\n", state);
+
 			}
 			if ((state==6 && (t==0) )  || ( state==13 && (t==0) ) || ( state==15 && (t==0) ) || ( state==16 && (t==0) )  ){
 				PORTB = (PORTB & ~PORTB) | (1<<GNS) | (1<<REW);  // VERDE  -   VERMELHO
@@ -118,6 +145,8 @@ int main(void)
 				tc1_init();
 				state=1;
 				pstate=1;
+				printf("\n estado %d -> VERDE - VERMELHO\n", state);
+
 			}
 			if (state==1 && (t==0)){
 				PORTB = ( PORTB ^ (1<<GNS) ) | (1<<YNS);        // AMARELO -   VERMELHO
@@ -125,6 +154,8 @@ int main(void)
 				tc1_init();
 				state=2;
 				pstate=2;
+				printf("\n estado %d -> AMARELO - VERMELHO\n", state);
+
 			}
 			if( state==2 && (t==0) ){
 				PORTB = ( PORTB ^ (1<<YNS) ) | (1<<RNS);        // VERMELHO -  VERMELHO
@@ -132,6 +163,8 @@ int main(void)
 				tc1_init();
 				state=3;
 				pstate=3;
+				printf("\n estado %d -> VERMELHO - VERMELHO\n", state);
+
 			}
 			if( (state==3 && (t==0) ) || (state==8 && (t==0)) || ( state==10 && (t==0)) || ( state==11 && (t==0))){
 				PORTB = ( PORTB ^ (1<<REW) ) | (1<<GEW);        // VERMELHO -  VERDE
@@ -139,6 +172,8 @@ int main(void)
 				tc1_init();
 				state=4;
 				pstate=4;
+				printf("\n estado %d -> VERMELHO - VERDE\n", state);
+
 			}
 			if(state==4 && (t==0)){
 				PORTB = ( PORTB ^ (1<<GEW) ) | (1<<YEW);        // VERMELHO - AMARELO
@@ -146,6 +181,8 @@ int main(void)
 				tc1_init();
 				state=5;
 				pstate=5;
+				printf("\n estado %d -> VERMELHO - AMARELO\n", state);
+
 			}
 			if(state==5 && (t==0) ){
 				PORTB = ( PORTB ^ (1<<YEW) ) | (1<<REW);        // VERMELHO - VERMELHO
@@ -153,6 +190,8 @@ int main(void)
 				tc1_init();
 				state=6;
 				pstate=6;
+				printf("\n estado %d -> VERMELHO - VERMELHO\n", state);
+
 			}
 	        /******************************* - EMERGENCIA - ********************************/
 			// estado 17
@@ -163,23 +202,28 @@ int main(void)
 				tc1_init();
 				state=7;
 				pstate=0;
+				printf("\nEMERGENCIA estado %d -> AMARELO - VERMELHO\n", state);
+
 			}
 			if(state==7 && (t==0)){
 				PORTB = ( PORTB ^ (1<<YNS) ) | (1<<RNS);        //VERMELHO - VERMELHO
 				t=mediumT;
 				tc1_init();
 				state=8;
+				printf("\nEMERGENCIA estado %d -> VERMELHO - VERMELHO\n", state);
 			}
 		   //estado 2
 			if( (state==17) && (pstate==2) ){			//AMARELO - VERMELHO
 				state=9;
 				pstate=0;
+				printf("\nEMERGENCIA estado %d -> AMARELO - VERMELHO\n", state);
 			}
 			if( (state==9) && (t==0)){
 				PORTB = ( PORTB ^ (1<<YNS) ) | (1<<RNS);	//VERMELHO - VERMELHO
 				t=mediumT;
 				tc1_init();
 				state=10;
+				printf("\nEMERGENCIA estado %d -> VERMELHO - VERMELHO\n", state);
 			}
 			//estado 3
 			if( (state==17) && (pstate==3)){
@@ -187,6 +231,7 @@ int main(void)
 				tc1_init();
 				state=11;
 				pstate=0;
+				printf("\nEMERGENCIA estado %d -> VERMELHO - VERMELHO\n", state);
 			}
 			// estado 4
 			if( (state==17) && (pstate==4)){
@@ -195,23 +240,27 @@ int main(void)
 				tc1_init();
 				state=12;
 				pstate=0;
+				printf("\nEMERGENCIA estado %d -> VERMELHO - AMARELO\n", state);
 			}
 			if(state==12 && (t==0)){
 				PORTB = ( PORTB ^ (1<<YEW) ) | (1<<REW);        //VERMELHO - VERMELHO
 				t=mediumT;
 				tc1_init();
 				state=13;
+				printf("\nEMERGENCIA estado %d -> VERMELHO - VERMELHO\n", state);
 			}
 		   //estado 5
-			if( (state==17) && (pstate==5)){			 //VERMELHO - AMARELO
+			if( (state==17) && (pstate==5)){			//VERMELHO - AMARELO
 				state=14;
 				pstate=0;
+				printf("\nEMERGENCIA estado %d -> VERMELHO - AMARELO\n", state);
 			}
 			if( (state==14) && (t==0)){
 				PORTB = ( PORTB ^ (1<<YEW) ) | (1<<REW);	//VERMELHO - VERMELHO
 				t=mediumT;
 				tc1_init();
 				state=15;
+				printf("\nEMERGENCIA estado %d -> VERMELHO - VERMELHO\n", state);
 			}
 			//estado 6
 			if( (state==17) && (pstate==6)){
@@ -219,8 +268,9 @@ int main(void)
 				tc1_init();
 				state=16;
 				pstate=0;
+				printf("\nEMERGENCIA estado %d -> VERMELHO - VERMELHO\n", state);
 			}
-	}
+		}
 
   return 1;
 }
